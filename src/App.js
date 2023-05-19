15d218e571b429d26eea4d6468023a75e6272da3
SCHADE JR <schade@SCHADEs-MacBook-Air.local>
2023-05-19 19:51:38 +0300

    categories

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './common/header/Header';
-import { BrowserRouter as Router, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Pages from './pages/Pages';
 
 function App() {
   return (
@@ -8,7 +9,10 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        {/* <Route exact path='/' element={<About />} /> */}
+        <Route exact path='/' element={<Pages />} />
+        {/* <Route path='/' exact>
+          <Pages />
+        </Route> */}
       </Routes>
     </Router>
    </>
